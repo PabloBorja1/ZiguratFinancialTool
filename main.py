865a3c8c9a5bf8c99e7c229aaadc4731b3e9067c
PabloBorja1 <128264401+PabloBorja1@users.noreply.github.com>
2023-03-23 20:55:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.fundamentaldata import FundamentalData
 from stocknews import StockNews
 import os
+from bs4 import BeautifulSoup
+import numpy as np
+import altair as alt
 
 with open('.env') as f:
     for line in f:
@@ -56,7 +59,7 @@ def analyze_balance_sheet(balance_sheet):
     # Call OpenAI API to analyze the balance sheet data
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"This is the balance sheet data from a company, use this balance sheet to make a financial analysis and give us two good things and two bad things about this company based in the statement:\n{balance_sheet}\n\n",
+        prompt=f"You are a Financial Analyst, this is the balance sheet from a company, first make an analysis about the company based in this data, and finally give us two good things and two bad things about this company based in the statement:\n{balance_sheet}\n\n",
         max_tokens=1024,
         n=1,
         stop=None,
@@ -64,7 +67,7 @@ def analyze_balance_sheet(balance_sheet):
     )
 
     # Extract the financial analysis from OpenAI API response
-    analysis = response.choices[0].text.strip()
+    analysis = response.choices[0].text
 
     return analysis
 
@@ -75,7 +78,7 @@ def analyze_cash_flow(cash_flow):
     # Call OpenAI API to analyze the cash flow data
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"This is the cash flow statement data from a company, use this cash flow to make a financial analysis and give us two good things and two bad things about this company based in the statement:\n{cash_flow}\n\n",
+        prompt=f"You are a Financial Analyst, this is the cash flow statement from a company, first make an analysis about the company based in this data, and finally give us two good things and two bad things about this company based in the statement:\n{cash_flow}\n\n",
         max_tokens=1024,
         n=1,
         stop=None,
@@ -83,7 +86,7 @@ def analyze_cash_flow(cash_flow):
     )
 
     # Extract the financial analysis from OpenAI API response
-    analysis = response.choices[0].text.strip()
+    analysis = response.choices[0].text
 
     return analysis
 
@@ -94,7 +97,7 @@ def analyze_income_statement(income):
     # Call OpenAI API to analyze the income data
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"This is the income statement data from a company, use this income statement to make a financial analysis and give us two good things and two bad things about this company based in the statement:\n{income}\n\n",
+        prompt=f"You are a Top Financial Analyst, this is the income statement from a company, first make an analysis about the company based in this data, and finally give us two good things and two bad things about this company based in the statement:\n{income}\n\n",
         max_tokens=1024,
         n=1,
         stop=None,
@@ -102,18 +105,18 @@ def analyze_income_statement(income):
     )
 
     # Extract the financial analysis from OpenAI API response
-    analysis = response.choices[0].text.strip()
+    analysis = response.choices[0].text
 
     return analysis
 
-def make_summary(variable,variable2):
+def make_summary(variable,stock):
     """
     Analyze  data using OpenAI API and return summary financial analysis.
     """
     # Call OpenAI API to analyze the income data
     response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Please make a summary about this financial analysis text\n{variable2}\n:\n{variable}\n\n",
+        engine="text-davinci-003",
+        prompt=f"You are a Financial Analyst, after reading the next financial summary of {stock}, first make an analysys on the financials of {stock}, and finally give us a comentary about the sentiment on buying or selling the stock:\n{variable}\n\n",
         max_tokens=1024,
         n=1,
         stop=None,
@@ -121,7 +124,7 @@ def make_summary(variable,variable2):
     )
 
     # Extract the financial analysis from OpenAI API response
-    analysis = response.choices[0].text.strip()
+    analysis = response.choices[0].text
 
     return analysis
 
@@ -144,6 +147,59 @@ def analyze_news(input):
 
     return analysis
 
+def analyze_insider(input):
+    """
+    Analyze income data using OpenAI API and return news analysis.
+    """
+    # Call OpenAI API to analyze the income data
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"You are a top Financial Analyst, this is the information of insider trading from company, first: use this information to make an analysis about what could be happening in the company, and finally end with a commentary about the overall sentiment of the insiders:\n{input}\n\n",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Extract the financial analysis from OpenAI API response
+    analysis = response.choices[0].text.strip()
+
+    return analysis
+
+def insider_trading_ticker(ti):
+    # Define the URL of the page to scrape
+    url = f'http://openinsider.com/screener?s={ti}&o=&pl=&ph=&ll=&lh=&fd=730&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=100&page=1'
+
+    # Send a GET request to the URL and store the response
+    response = requests.get(url)
+
+    # Parse the response with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find the table containing the insider trading data
+    table = soup.find('table', {'class': 'tinytable'})
+
+    # Extract the data from the table
+    data = []
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [col.text.strip() for col in cols]
+        data.append(cols)
+
+    # Print the data
+    df = pd.DataFrame(data)
+    df.columns = ['x', 'Filing Date', 'Trade Date', 'Ticker', 'Insider Name', 'Title', 'Trade Type', 'Price', 'Qty',
+                  'Owned', 'ΔOwn', 'Value', '1d', '1w', '1m', '9m']
+    df = df[['Trade Date', 'Ticker', 'Insider Name', 'Title', 'Trade Type', 'Price', 'Qty', 'Owned', 'ΔOwn', 'Value']]
+    df = df.drop(index=0)
+    df['Value'] = df['Value'].str.replace('$', '')
+    df['Value'] = df['Value'].str.replace(',', '')
+    df['Value'] = pd.to_numeric(df['Value'])
+    df['Type'] = np.where(df['Value'] > 0, 'buys', 'sales')
+    df['Trade Date'] = pd.to_datetime(df['Trade Date'])
+    return df
+
 def calculate_changes(data):
     close = data['Close']
     current_price = close.iloc[-1]
@@ -159,6 +215,13 @@ def calculate_changes(data):
         year_change = 0
     return round(current_price, 2), round(day_change, 2), round(week_change, 2), round(month_change, 2), round(
         year_change, 2)
+
+def map_color(val):
+    if val >= 0:
+        return 'green'
+    else:
+        return 'red'
+
 
 def main():
     st.set_page_config(page_title='Ticker Reports', page_icon=':chart_with_upwards_trend:')
@@ -251,7 +314,7 @@ def main():
             yahooticker = yf.Ticker(symbol)
 
             # Tabs for type of holder:
-            slide1, slide3, slide2 = st.tabs(["Overview", "News", "Analysis"])
+            slide1, slide3, slide2, slide4 = st.tabs(["Overview", "News", "Analysis", "Insider Trading"])
 
             with slide1:
                 # Create a button to generate the report
@@ -334,7 +397,7 @@ def main():
                         # OPEN AI summary request
                         resume_balance_sheet = analyze_balance_sheet(data)
                         # Display the balance sheet data summary
-                        st.write(resume_balance_sheet)
+                        st.text(resume_balance_sheet)
                         data = data.applymap(lambda x: str(round(x/1000000, 2)) + 'm' if isinstance(x, (int, float)) else x)
                         with st.expander("Balance Sheet"):
                             st.write(data)
@@ -347,7 +410,7 @@ def main():
                         # OPEN AI summary request
                         resume_cash_flow = analyze_cash_flow(data)
                         # Display the balance sheet data summary
-                        st.write(resume_cash_flow)
+                        st.text(resume_cash_flow)
                         numeric_cols = data.select_dtypes(include=['number']).columns
                         data = data.applymap(lambda x: str(round(x/1000000, 2)) + 'm' if isinstance(x, (int, float)) else x)
                         with st.expander("Cash Flow"):
@@ -361,18 +424,20 @@ def main():
                         # OPEN AI summary request
                         resume_income = analyze_income_statement(data)
                         # Display the balance sheet data summary
-                        st.write(resume_income)
+                        st.text(resume_income)
                         numeric_cols = data.select_dtypes(include=['number']).columns
                         data = data.applymap(lambda x: str(round(x/1000000, 2)) + 'm' if isinstance(x, (int, float)) else x)
                         with st.expander("Income"):
                             st.write(data)
 
-                        all_financials = resume_balance_sheet + resume_cash_flow + resume_income
-                        final_summary = make_summary(all_financials, symbol)
+                        all_financials = "Balance Sheet: " + resume_balance_sheet + "Cash Flow: " + resume_cash_flow + "Income Statement: " + resume_income
+                        final_summary = make_summary(all_financials, tickername)
 
                         st.write("Summary")
                         st.write("")
                         st.write(final_summary)
+                        st.write("This is not Financial Advice, but you can contact Zigurat Capital for Extreme profits.")
+                        st.write("Cheers, Financial AI.")
 
             with slide3:
                 # Extract yfinance
@@ -402,6 +467,32 @@ def main():
 
                 with st.expander("Latests News"):
                     st.markdown(yahoonews.to_html(render_links=True, escape=False), unsafe_allow_html=True, )
+
+            with slide4:
+                #Get Insider Data
+                insider_data = insider_trading_ticker(symbol)
+                # Display Ticker Name:
+                tickername = r.json()['Name']
+                st.subheader(tickername)
+
+                insider_analysis = analyze_insider(insider_data)
+                st.write(insider_analysis)
+
+                #Make a chart
+                c = alt.Chart(insider_data).mark_bar().encode(
+                    x="Trade Date",
+                    y="Value",
+                    color=alt.condition(
+                        alt.datum.Value > 0,
+                        alt.value("green"),  # The positive color
+                        alt.value("red")  # The negative color
+                    )
+                ).properties(width=600)
+
+                st.altair_chart(c, use_container_width=True)
+
+                with st.expander("Insider Trading Data"):
+                    st.table(insider_data)
 
     # If News or Fundamental Analysis is selected, display message
     else:
